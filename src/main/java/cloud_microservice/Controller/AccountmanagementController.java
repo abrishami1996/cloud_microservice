7bc774cba5ee711c48b6ba881f251227fc3347e1
Mahdi <abrishami.mahdi@gmail.com>
2020-01-06 23:08:33 +0330

    First API Added!

---
+++ src/main/java/cloud_microservice/Controller/AccountmanagementController.java
@@ -0,0 +1,22 @@
+package cloud_microservice.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+
+@RestController
+public class AccountmanagementController {
+
+    @RequestMapping(value = "/HeartBeat",method = RequestMethod.GET)
+    public String addCourse() {
+
+        return "Account Management is up and running!";
+
+    }
+}
--- src/main/java/cloud_microservice/Model/Transaction.java
+++ src/main/java/cloud_microservice/Model/Transaction.java
@@ -6,6 +6,7 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Id
     @OneToOne
     @PrimaryKeyJoinColumn
