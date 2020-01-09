885d6fba7ecab4b3a2d725e30ffc4d02d2d14aee
Mahdi <abrishami.mahdi@gmail.com>
2020-01-09 10:36:47 +0330

    Update Profile added!

--- src/main/java/cloud_microservice/Controller/AccountmanagementController.java
+++ src/main/java/cloud_microservice/Controller/AccountmanagementController.java
@@ -18,7 +18,7 @@ public class AccountmanagementController {
     @Autowired
     ProfileRepository profilerepository;
 
-    @RequestMapping(value = "/account/HeartBeat",method = RequestMethod.GET)
+    @RequestMapping(value = "/account/HeartBeat", method = RequestMethod.GET)
     public String HeartBeat() {
 
         return "Account Management is up and running!";
@@ -28,9 +28,8 @@ public class AccountmanagementController {
 //    @Value("localhost:")
 //    private String AuthService;
 
-    @RequestMapping(value = "/account/CreateProfile",method = RequestMethod.POST)
-    private String Createprofile(@RequestBody ProfileRequest req){
-
+    @RequestMapping(value = "/account/CreateProfile", method = RequestMethod.POST)
+    private String Createprofile(@RequestBody ProfileRequest req) {
         String url = "This must be the docker container address";
         RestTemplate restTemplate = new RestTemplate();
         Map<String, String> map = new HashMap<>();
@@ -40,11 +39,11 @@ public class AccountmanagementController {
         HttpStatus statusCode = response.getStatusCode();
         String token = response.getBody().getToken();
         // check response
-        if ( statusCode.value() == 201 ) {
-            Profile profile = new Profile(req.getEmail(), req.getName(),req.getPhoneNo(),req.getNationalCode(),req.getAddress(),req.getPostalCode());
+        if (statusCode.value() == 201) {
+            Profile profile = new Profile(req.getEmail(), req.getName(), req.getPhoneNo(), req.getNationalCode(), req.getAddress(), req.getPostalCode());
             profilerepository.save(profile);
             return token;
-        } else if ( statusCode.value() == 400 ) {
+        } else if (statusCode.value() == 400) {
             return ("Invalid Parameters");
         } else {
             return "Email Already Exists!";
@@ -52,20 +51,39 @@ public class AccountmanagementController {
 
     }
 
-    private Boolean Validator (String jwttoken){
+    private Boolean Validator(String jwttoken) {
         final String uri = ".../authentiq/v1/validate/token";
         System.out.println(jwttoken);
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(jwttoken);
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        entity = restTemplate.exchange(uri, HttpMethod.GET,entity,String.class);
+        entity = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
         HttpStatus statusCode = ((ResponseEntity<String>) entity).getStatusCode();
-        if (statusCode.value() == 200)
-        {
+        if (statusCode.value() == 200) {
             return true;
         }
         return false;
 
     }
+
+    @RequestMapping(value = "/account/UpdateProfile",method = RequestMethod.POST)
+    public String UpdateProfile(String jwttoken, @RequestBody ProfileRequest req){
+        if (Validator(jwttoken)) {
+            Profile p = profilerepository.findByEmail(req.getEmail());
+            if ( p!= null) {
+                p.setName(req.getName());
+                p.setAddress(req.getAddress());
+                p.setNationalCode(req.getNationalCode());
+                p.setPhoneNo(req.getPhoneNo());
+                p.setPostalCode(req.getPostalCode());
+                profilerepository.save(p);
+                return "ok";
+            }
+            return "Not Found";
+        }
+        return "not authenticated";
+
+    }
 }
+
--- src/main/java/cloud_microservice/Repository/ProfileRepository.java
+++ src/main/java/cloud_microservice/Repository/ProfileRepository.java
@@ -6,12 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import javax.transaction.Transactional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-//    boolean existsByStudentNumber(long studentNumber);
-//    boolean existsById(int ID);
-//    @Transactional
-//    void deleteByStudentNumber(long StudentNumber);
-//    @Transactional
-//    void deleteById(int Id);
-//    Student findById(int fk);
+
+    Profile findByEmail(String email);
+    boolean existsByEmail(String email);
 
 }
